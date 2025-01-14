05b8d8ae2188b72a8d905699b4cd78ef57c236bb
Eduardo Ramos <eduardodossantosramos@hotmail.com>
2025-01-14 12:14:43 +0000

    First Commit

---
+++ src/main/java/br/com/devpod/example/poc_devpod/PocDevpodApplication.java
@@ -0,0 +1,13 @@
+package br.com.devpod.example.poc_devpod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocDevpodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocDevpodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devpod/example/poc_devpod/PocDevpodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devpod.example.poc_devpod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocDevpodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
